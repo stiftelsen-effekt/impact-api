7acdf94c700c294bfb4473a614f3ba668ca77c1e
Arepo <jinksy@gmail.com>
2022-04-28 19:26:27 +0200

    Evaluation toy lookup working

--- impact_api/api/urls.py
+++ impact_api/api/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('evaluations/<str:charity_name>/<int:year>', views.evaluation, name='evaluation'),
+    path('distributions/<int:year>/<int:month>', views.max_impact_distribution, name='max_impact_distribution')
 ]
--- impact_api/api/views.py
+++ impact_api/api/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Evaluation
+from datetime import date
 
 def index(request):
     return HttpResponse("Hello, world. You're at the api index.")
 
-# def read_evaluation(request, charity, start_date):
+def evaluation(request, charity_name, year):
+    # e = Evaluation.get(charity_name=charity_name)
+    start_date = date(year, 1, 1)
+    evaluation = Evaluation.objects.get(
+                                        charity_name=charity_name,
+                                        start_date=start_date)
+    response = f"You're looking at this evaluation {evaluation}"
+    return HttpResponse(response)
 
+def max_impact_distribution(request, year, month):
+    pass
