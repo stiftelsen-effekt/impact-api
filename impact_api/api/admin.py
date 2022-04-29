e0e84183f4369d751ca8c655ec7a78dc931e06e5
Arepo <jinksy@gmail.com>
2022-04-29 19:25:47 +0200

    Restructure database

--- impact_api/api/admin.py
+++ impact_api/api/admin.py
@@ -1,20 +1,27 @@
 from django.contrib import admin
 
-from .models import MaxImpactDistribution, Evaluation
+from .models import MaxImpactDistribution, Evaluation, Grant
 
-class EvaluationInline(admin.TabularInline):
-    model = Evaluation
+class GrantInline(admin.StackedInline):
+    model = Grant
     choice = 5
 
 class EvaluationAdmin(admin.ModelAdmin):
-    list_display = ('charity_name', 'start_year', 'start_month', 'cents_per_output', 'output_type')
-    search_fields = ['charity_name', 'output_type']
+    list_display = (
+        'charity_name', 'start_year', 'start_month',
+        'cents_per_output', 'short_output_description')
+    search_fields = [
+        'charity_name', 'short_output_description',
+        'long_output_description']
 
 class MaxImpactDistributionAdmin(admin.ModelAdmin):
-    inlines = [EvaluationInline]
-
+    inlines = [GrantInline]
+    search_fields = [
+        'charity_name', 'short_output_description',
+        'long_output_description']
 
 admin.site.register(MaxImpactDistribution, MaxImpactDistributionAdmin)
 admin.site.register(Evaluation, EvaluationAdmin)
+admin.site.register(Grant)
 
 # Register your models here.
--- impact_api/api/models.py
+++ impact_api/api/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.template.defaultfilters import slugify
 
 def validate_year(value):
-    if value not in range(2000,3000):
+    if value not in range(2000,2100):
         raise ValidationError(
             _('must be the year of a Givewell evalution'),
             params={'value': value},
@@ -17,39 +20,39 @@ def validate_month(value):
             params={'value': value},
         )
 
-
 class MaxImpactDistribution(models.Model):
     def __str__(self):
         return f'{self.start_year}-{self.start_month}'
     start_year = models.IntegerField(validators=[validate_year])
     start_month = models.IntegerField(validators=[validate_month])
+    # has_many Grants
 
-    end_year = models.IntegerField(blank=True, null=True, validators=[validate_year]) # TODO must be after start_date
-    end_month = models.IntegerField(blank=True, null=True, validators=[validate_month]) # TODO (not mandatory,
-    # though maybe we want some validation that only one Distribution
-    # lacks this); if present, must be after_start date
-
-    # has_many Evaluations
+class Grant(models.Model):
+    def __str__(self):
+        return f"${self.grant_in_cents / 100} to {self.charity_name}"
+    max_impact_distribution = models.ForeignKey(
+        MaxImpactDistribution, on_delete=models.CASCADE)
+    charity_name = models.CharField(max_length=200)
+    charity_abbreviation = models.CharField(
+        max_length=10, blank=True, null=True)
+    short_output_description = models.CharField(max_length=100)
+    long_output_description = models.CharField(max_length=1000)
+    grant_in_cents = models.IntegerField()
+    number_outputs_purchased = models.IntegerField()
 
 class Evaluation(models.Model):
     def __str__(self):
         return f'{self.charity_name} starting {self.start_year}-{self.start_month}'
-    max_impact_distribution = models.ForeignKey(
-        MaxImpactDistribution, on_delete=models.CASCADE,
-        blank=True, null=True)
+    charity_abbreviation = models.CharField(max_length=10)
     charity_name = models.CharField(max_length=200)
-    start_year = models.IntegerField(validators=[validate_year]) # TODO dates are mandatory unless it
-    # belongs to a Distribution
+    start_year = models.IntegerField(validators=[validate_year])
     start_month = models.IntegerField(validators=[validate_month])
-
-    end_year = models.IntegerField(blank=True, null=True, validators=[validate_year]) # TODO must be after start_date
-    end_month = models.IntegerField(blank=True, null=True, validators=[validate_month])
+    short_output_description = models.CharField(max_length=100)
+    long_output_description = models.CharField(max_length=1000)
     cents_per_output = models.IntegerField()
-    output_type = models.CharField(max_length=511)
-    cents_donated = models.IntegerField(default=100) # only set
-    # for Evaluations that belong to Distributions;
+    # TODO - make Evaluations unique by (charity name and date)
 
-    # Evaluations are unique by (charity name and date/date range) - or
-    # not if they might cover multiple interventions in the same year by
-    # the same org
+@receiver(pre_save, sender=Evaluation)
+def capitalize_abbreviation(sender, instance, *args, **kwargs):
+    instance.charity_abbreviation = instance.charity_abbreviation.upper()
 
