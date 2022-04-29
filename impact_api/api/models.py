681563611fe5de6bfd12ca2b826286c864644b58
Arepo <jinksy@gmail.com>
2022-04-29 11:14:37 +0200

    Add month and year validations

--- impact_api/api/models.py
+++ impact_api/api/models.py
@@ -1,8 +1,26 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+def validate_year(value):
+    if value not in range(2000,3000):
+        raise ValidationError(
+            _('year must be the year of a Givewell evalution'),
+            params={'value': value},
+        )
+
+def validate_month(value):
+    # TODO - find out best practice here. Move to a validators file?
+    if value not in range(1, 13):
+        raise ValidationError(
+            _('month must be a number from 1-12'),
+            params={'value': value},
+        )
+
 
 class MaxImpactDistribution(models.Model):
     def __str__(self):
-        return f'{self.start_date}'
+        return f'{self.start_year}-{self.start_month}'
     start_date = models.DateField() # mandatory
     end_date = models.DateField(blank=True, null=True) # (not mandatory,
     # though maybe we want some validation that only one Distribution
@@ -12,14 +30,16 @@ class MaxImpactDistribution(models.Model):
 
 class Evaluation(models.Model):
     def __str__(self):
-        return f'{self.charity_name} starting {self.start_date}'
+        return f'{self.charity_name} starting {self.start_year}-{self.start_month}'
     max_impact_distribution = models.ForeignKey(
         MaxImpactDistribution, on_delete=models.CASCADE,
         blank=True, null=True)
     charity_name = models.CharField(max_length=200)
-    start_date = models.DateField() # dates are mandatory unless it
+    start_year = models.IntegerField(validators=[validate_year]) # dates are mandatory unless it
+    start_month = models.IntegerField(validators=[validate_month])
     # belongs to a Distribution
-    end_date = models.DateField(blank=True, null=True) # must be after start_date
+    end_year = models.IntegerField(blank=True, null=True, validators=[validate_year]) # must be after start_date
+    end_month = models.IntegerField(blank=True, null=True, validators=[validate_month])
     cents_per_output = models.IntegerField() # mandatory
     output_type = models.CharField(max_length=511)
     cents_donated = models.IntegerField(default=100) # only set
@@ -28,3 +48,4 @@ class Evaluation(models.Model):
     # Evaluations are unique by (charity name and date/date range) - or
     # not if they might cover multiple interventions in the same year by
     # the same org
+
