a8b852724dac53f57698f0d9fd8417ebfb8e6d20
Arepo <jinksy@gmail.com>
2022-05-09 12:57:19 +0200

    Add first test

--- impact_api/api/tests.py
+++ impact_api/api/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
+from .models import Allotment
+
+class AllotmentModelTests(TestCase):
+    def test_rounds_cents_per_output_upward_correctly(self):
+        """
+            Returns an integer value for number of cents per output
+        """
+        allotment = Allotment(sum_in_cents=5, number_outputs_purchased=3)
+        self.assertIs(allotment.rounded_cents_per_output(), 2)
+
+    def test_rounds_cents_per_output_downward_correctly(self):
+        """
+            Returns an integer value for number of cents per output
+        """
+        allotment = Allotment(sum_in_cents=5, number_outputs_purchased=4)
+        self.assertIs(allotment.rounded_cents_per_output(), 1)
+
 # Create your tests here.
