7ff6086864a3fe6b371bd455685e9056e9fc4b42
Arepo <jinksy@gmail.com>
2022-05-10 16:14:55 +0200

    Add some Max Impact Fund Grant view tests

--- impact_api/api/tests.py
+++ impact_api/api/tests.py
@@ -6,14 +6,14 @@ import json
 from .models import (
     Allotment, Evaluation, MaxImpactFundGrant, Charity, Intervention)
 
-def create_charity(charity_name='Skynet', abbreviation='SN') -> Charity:
+def create_charity(charity_name='Skynet', abbreviation='SN'):
     return Charity.objects.create(
         charity_name=charity_name, abbreviation=abbreviation)
 
 def create_intervention(
         short_output_description='Distributing killer robots',
         long_output_description=('Working to reduce the risk from '
-                                 'misaligned natural intelligence')) -> Intervention:
+                                 'misaligned natural intelligence')):
     return Intervention.objects.create(
         short_output_description=short_output_description,
         long_output_description=long_output_description)
@@ -26,6 +26,22 @@ def create_evaluation(start_year=2010, start_month=12, cents_per_output=100,
         start_month=start_month, start_year=start_year,
         cents_per_output=cents_per_output, charity=charity,
         intervention=intervention)
+
+def create_grant(start_year=2015, start_month=6) -> MaxImpactFundGrant:
+    return MaxImpactFundGrant.objects.create(
+        start_year=start_year, start_month=start_month)
+
+def create_allotment(
+    sum_in_cents=9999, number_outputs_purchased=2222,
+    charity=None, intervention=None, max_impact_fund_grant=None):
+    charity = charity or create_charity()
+    intervention = intervention or create_intervention()
+    max_impact_fund_grant = max_impact_fund_grant or create_grant()
+    return Allotment.objects.create(
+        sum_in_cents=sum_in_cents,
+        number_outputs_purchased=number_outputs_purchased,
+        charity=charity, intervention=intervention,
+        max_impact_fund_grant=max_impact_fund_grant)
 
 class AllotmentModelTests(TestCase):
     def test_rounds_cents_per_output_upward_correctly(self):
@@ -45,6 +61,10 @@ class AllotmentModelTests(TestCase):
         self.assertIs(allotment.rounded_cents_per_output(), 1)
 
 class EvaluationViewTests(TestCase):
+    def setUp(self):
+        if self._testMethodName != 'test_no_evaluations':
+            self.eval_1 = create_evaluation()
+
     def test_no_evaluations(self):
         response = self.client.get(reverse('evaluations'))
         self.assertIs(response.status_code, 200)
@@ -52,25 +72,23 @@ class EvaluationViewTests(TestCase):
         self.assertEqual(content['evaluations'], [])
 
     def test_one_evaluation(self):
-        create_evaluation()
         response = self.client.get(reverse('evaluations'))
         content = json.loads(response.content)
         self.assertEqual(content['evaluations'],
-            [{'cents_per_output': 100,
+            [{'id': 1,
+              'cents_per_output': 100,
+              'start_month': 12,
+              'start_year': 2010,
               'charity': {
                   'abbreviation': 'SN', 'charity_name': 'Skynet', 'id': 1},
-              'id': 1,
               'intervention': {
                   'id': 1,
                   'short_output_description': 'Distributing killer robots',
                   'long_output_description': (
                       'Working to reduce the risk from misaligned '
-                      'natural intelligence')},
-              'start_month': 12,
-              'start_year': 2010}])
+                      'natural intelligence')}}])
 
     def test_filtering_by_charity(self):
-        create_evaluation()
         charity = create_charity(
             charity_name='Impossible Meat', abbreviation='IM')
         intervention = create_intervention(
@@ -94,9 +112,9 @@ class EvaluationViewTests(TestCase):
             'Skynet')
 
     def test_filtering_by_year(self):
-        eval_1 = create_evaluation()
-        eval_2 = create_evaluation(
-            charity=eval_1.charity, intervention=eval_1.intervention,
+        create_evaluation(
+            charity=self.eval_1.charity,
+            intervention=self.eval_1.intervention,
             start_year=2011)
         query_1 = reverse('evaluations') + '?start_year=2011'
         content_1 = json.loads(self.client.get(query_1).content)
@@ -112,30 +130,70 @@ class EvaluationViewTests(TestCase):
             2010)
 
     def test_filtering_within_year(self):
-        eval_1 = create_evaluation()
-        eval_2 = create_evaluation(
-            charity=eval_1.charity, intervention=eval_1.intervention,
+        create_evaluation(
+            charity=self.eval_1.charity,
+            intervention=self.eval_1.intervention,
             start_month=1)
         query_1 = reverse('evaluations') + '?start_month=6'
         content_1 = json.loads(self.client.get(query_1).content)
         query_2 = reverse('evaluations') + '?end_month=6'
         content_2 = json.loads(self.client.get(query_2).content)
-        self.assertEqual(len(content_1), 1)
-        self.assertEqual(len(content_2), 1)
-        self.assertEqual(
-            content_1['evaluations'][0]['start_month'],
-            12)
-        self.assertEqual(
-            content_2['evaluations'][0]['start_month'],
-            1)
+        self.assertEqual(len(content_1['evaluations']), 1)
+        self.assertEqual(len(content_2['evaluations']), 1)
+        self.assertEqual(content_1['evaluations'][0]['start_month'], 12)
+        self.assertEqual(content_2['evaluations'][0]['start_month'], 1)
 
 
 class MaxImpactFundGrantIndexViewTests(TestCase):
-    def test_no_evaluations(self):
+    def setUp(self):
+        if self._testMethodName != 'test_no_grants':
+            self.grant_1 = create_grant()
+            self.allotment_1 = create_allotment(
+                max_impact_fund_grant=self.grant_1)
+
+    def test_no_grants(self):
         response = self.client.get(reverse('max_impact_fund_grants'))
         self.assertIs(response.status_code, 200)
         content = json.loads(response.content)
         self.assertEqual(content['max_impact_fund_grants'], [])
+
+    def test_one_grant(self):
+        response = self.client.get(reverse('max_impact_fund_grants'))
+        content = json.loads(response.content)
+        self.assertEqual(content['max_impact_fund_grants'],
+            [{'id': 1,
+              'start_year': 2015,
+              'start_month': 6,
+              'allotment_set': [{
+                  'id': 1,
+                  'cents_per_output': 4,
+                  'sum_in_cents': 9999,
+                  'number_outputs_purchased': 2222,
+                  'charity': {
+                      'id': 1,
+                      'charity_name': 'Skynet',
+                      'abbreviation': 'SN'},
+                  'intervention': {
+                      'id': 1,
+                      'short_output_description': 'Distributing killer robots',
+                      'long_output_description': ('Working to reduce the '
+                          'risk from misaligned natural intelligence')}}]}])
+
+    def test_filtering_by_year(self):
+        grant_2 = create_grant(start_year=2016)
+        query_1 = reverse('max_impact_fund_grants') + '?start_year=2016'
+        content_1 = json.loads(self.client.get(query_1).content)
+        query_2 = reverse('max_impact_fund_grants') + '?end_year=2015'
+        content_2 = json.loads(self.client.get(query_2).content)
+        query_3 = reverse('max_impact_fund_grants')
+        content_3 = json.loads(self.client.get(query_3).content)
+        self.assertEqual(len(content_1['max_impact_fund_grants']), 1)
+        self.assertEqual(len(content_2['max_impact_fund_grants']), 1)
+        self.assertEqual(len(content_3['max_impact_fund_grants']), 2)
+        self.assertEqual(content_1['max_impact_fund_grants'][0]['start_year'], 2016)
+        self.assertEqual(content_2['max_impact_fund_grants'][0]['start_year'], 2015)
+
+
 
 
 
