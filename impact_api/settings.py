96ea5d4d82e33b4c8f3380ef9d47cb4ede710af6
Håkon Harnes <account@harnes.me>
2022-06-10 17:57:49 +0200

    Adds static files

--- impact_api/settings.py
+++ impact_api/settings.py
@@ -132,6 +132,8 @@ LANGUAGES = (
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
