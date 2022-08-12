43add58792d0621f0eb10bb3343b231330366d91
Arepo <jinksy@gmail.com>
2022-08-12 13:26:06 +0200

    Allow querying and conversion by donation dates

--- api/serializers.py
+++ api/serializers.py
@@ -17,10 +17,32 @@ class CurrencyManager():
         'en': 'USD'
     }
 
-    def get_converted_value(self, context, original_value, conversion_date):
-        '''Get latest currency conversions and return cost per output in
-        specified currency, else in USD'''
+    def get_converted_value(self, context, original_value, model_instance):
+        '''Get latest conversion on relevant date and return cost per
+        output in specified currency, else in USD'''
+        return self._get_exchange_rate_and_date(
+            context, model_instance, original_value)['converted_value']
+
+    def get_actual_exchange_rate_date(self, context, model_instance):
+        '''Get the actual date the currency was converted on, after
+        adjusting for days where it wasn't available'''
+        return self._get_exchange_rate_and_date(
+            context, model_instance)['conversion_date']
+
+    def _targeted_conversion_date(self, context, model_instance):
+        if context.get('donation_year'):
+            attempted_conversion_date = date(
+                int(context.get('donation_year')),
+                int(context.get('donation_month', 1)),
+                int(context.get('donation_day', 1)))
+        else:
+            attempted_conversion_date = model_instance.start_date()
+        return attempted_conversion_date
+
+
+    def _get_exchange_rate_and_date(self, context, model_instance, original_value=1):
         filename = f"currency_conversions/ecb_{date.today():%Y%m%d}.zip"
+        conversion_date = self._targeted_conversion_date(context, model_instance)
 
         if not op.isfile(filename):
             # If the next line raises an SSL error, following these steps might help:
@@ -31,20 +53,24 @@ class CurrencyManager():
 
         currency_code = (context.get('currency')
                          or self.DEFAULT_LANGUAGE_CURRENCY_MAPPING[get_language()]).upper()
-        try:
-            # ECB doesn't record conversion rates for weekend or some holiday dates, so if we don't
-            # have a match, try again for the day four days earlier (to dodge around Easter weekend)
-            return c.convert(
-                original_value / 100,
-                'USD',
-                currency_code,
-                conversion_date)
-        except RateNotFoundError:
-            return c.convert(
-                original_value / 100,
-                'USD',
-                currency_code,
-                conversion_date - timedelta(days=4))
+
+        converted_value = None
+        day_difference = 0
+        while not converted_value:
+            try:
+                # ECB doesn't record conversion rates for weekend or some holiday dates, so if we
+                # don't have a match, try again for the day four days earlier (to dodge around
+                # Easter weekend)
+                converted_value = c.convert(
+                    original_value / 100,
+                    'USD',
+                    currency_code,
+                    conversion_date - timedelta(day_difference))
+            except RateNotFoundError:
+                day_difference += 1
+        return {'conversion_date': conversion_date - timedelta(day_difference),
+                'converted_value': converted_value}
+
 
     def get_currency(self, context):
         '''Return specified currency, else USD'''
@@ -56,17 +82,55 @@ class InterventionSerializer(serializers.ModelSerializer):
         model = Intervention
         fields = ['long_description', 'short_description', 'id']
 
+class AllotmentSerializer(serializers.ModelSerializer):
+    intervention = InterventionSerializer(read_only=True)
+    converted_sum = serializers.SerializerMethodField()
+    currency = serializers.SerializerMethodField()
+    converted_cost_per_output = serializers.SerializerMethodField()
+    exchange_rate_date = serializers.SerializerMethodField()
+    manager = CurrencyManager()
+
+    def get_converted_cost_per_output(self, allotment) -> str:
+        '''Return cost per output in specified currency, else in USD'''
+        return self.manager.get_converted_value(
+            self.context, allotment.cents_per_output(), allotment)
+
+    def get_converted_sum(self, allotment):
+        '''Return sum in specified currency, else in USD'''
+        return self.manager.get_converted_value(
+            self.context, allotment.sum_in_cents, allotment)
+
+    def get_currency(self, allotment):
+        '''Return specified currency, else USD'''
+        return self.manager.get_currency(self.context)
+
+    def get_exchange_rate_date(self, allotment):
+        '''Return the actual date from which we're taking conversions'''
+        return self.manager.get_actual_exchange_rate_date(
+            self.context, allotment)
+
+    class Meta:
+        model = Allotment
+        exclude = ['max_impact_fund_grant']
+        depth = 1
+
 class EvaluationSerializer(serializers.ModelSerializer):
     intervention = InterventionSerializer(read_only=True)
     converted_cost_per_output = serializers.SerializerMethodField()
+    exchange_rate_date = serializers.SerializerMethodField()
     currency = serializers.SerializerMethodField()
     language = serializers.SerializerMethodField()
     manager = CurrencyManager()
 
     def get_converted_cost_per_output(self, evaluation):
         '''Return cost per output in specified currency, else in USD'''
-        conversion_date = evaluation.start_date()
-        return self.manager.get_converted_value(self.context, evaluation.cents_per_output, conversion_date)
+        return self.manager.get_converted_value(
+            self.context, evaluation.cents_per_output, evaluation)
+
+    def get_exchange_rate_date(self, evaluation):
+        '''Return the actual date from which we're taking conversions'''
+        return self.manager.get_actual_exchange_rate_date(
+            self.context, evaluation)
 
     def get_currency(self, evaluation):
         '''Return specified currency, else USD'''
@@ -81,41 +145,11 @@ class EvaluationSerializer(serializers.ModelSerializer):
         fields = '__all__'
         depth = 1
 
-class AllotmentSerializer(serializers.ModelSerializer):
-    intervention = InterventionSerializer(read_only=True)
-    converted_sum = serializers.SerializerMethodField()
-    currency = serializers.SerializerMethodField()
-    converted_cost_per_output = serializers.SerializerMethodField()
-
-    manager = CurrencyManager()
-
-    def get_converted_cost_per_output(self, allotment) -> str:
-        '''Return cost per output in specified currency, else in USD'''
-        return self.manager.get_converted_value(
-            self.context, allotment.cents_per_output(), self._conversion_date(allotment))
-
-    def get_converted_sum(self, allotment):
-        '''Return sum in specified currency, else in USD'''
-        return self.manager.get_converted_value(
-            self.context, allotment.sum_in_cents, self._conversion_date(allotment))
-
-    def get_currency(self, allotment):
-        '''Return specified currency, else USD'''
-        return self.manager.get_currency(self.context)
-
-    def _conversion_date(self, allotment) -> date:
-        return allotment.start_date()
-
-    class Meta:
-        model = Allotment
-        exclude = ['max_impact_fund_grant']
-        depth = 1
-
 class MaxImpactFundGrantSerializer(serializers.ModelSerializer):
     allotment_set = AllotmentSerializer(many=True)
     language = serializers.SerializerMethodField()
 
-    def get_language(self, allotment):
+    def get_language(self, grant):
         '''Return globally set language'''
         return get_language()
 
--- api/views.py
+++ api/views.py
@@ -1,10 +1,11 @@
 from collections import namedtuple
 from datetime import date
+from typing import Callable
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.db.models import Q
 from .models import Evaluation, MaxImpactFundGrant, Charity
-from .serializers import (
-    InterventionSerializer, EvaluationSerializer, AllotmentSerializer, MaxImpactFundGrantSerializer)
+from .serializers import EvaluationSerializer, MaxImpactFundGrantSerializer
 
 # Before any of the views are called, the code in middleware.py will run
 
@@ -18,31 +19,24 @@ def evaluations(request):
     start_month=<1-12>
     end_year=<2000-present>
     end_month=<1-12>
+    donation_year=<any year, though will only return a non-empty result if >= earliest evaluation>
+    donation_month=<1-12>
+    donation_day=<1-31>
     charity_abbreviation=AMF&charity_abbreviation=sci&charity_abbreviation=DtW
     The latter uses all supplied charity codes (and is case-insensitive for
     ascii characters)
     language=<i18n country code>
     currency=<ISO 4217 code>
     '''
-    queries = request.GET
-    dates = _get_dates(queries)
-
-    charity_abbreviations = [
-        abbreviation.upper()
-        for abbreviation in queries.getlist('charity_abbreviation')] or (
-            Charity.objects.values_list('abbreviation', flat=True))
-
-    evaluations = Evaluation.objects.filter(
-        charity__abbreviation__in=charity_abbreviations,
-        start_year__gte=dates.start_year,
-        start_month__gte=dates.start_month,
-        start_year__lte=dates.end_year,
-        start_month__lte=dates.end_month)
-
-    response = {'evaluations': [
-        EvaluationSerializer(evaluation, context=queries).data for evaluation in evaluations]}
-    if not evaluations:
-        response['warnings'] = ['No evaluations found with those parameters']
+    query_strings = request.GET
+    charities_query = Q(charity__abbreviation__in=_get_charity_abbreviations(query_strings))
+    response = _construct_response(
+        query_strings,
+        Evaluation,
+        'evaluations',
+        EvaluationSerializer,
+        _get_evaluations_by_donation_date,
+        charities_query)
     return JsonResponse(response)
 
 def max_impact_fund_grants(request):
@@ -56,33 +50,78 @@ def max_impact_fund_grants(request):
     start_month=<1-12>
     end_year=<2000-present>
     end_month=<1-12>
+    donation_year=<any year, though will only return a non-empty result if >= earliest evaluation>
+    donation_month=<1-12>
+    donation_day=<1-31>
     language=<i18n country code>
     currency=<ISO 4217 code>
     '''
-    queries = request.GET
-    dates = _get_dates(queries)
 
-    grants = MaxImpactFundGrant.objects.filter(
+    query_strings = request.GET
+
+    response = _construct_response(
+        query_strings,
+        MaxImpactFundGrant,
+        'grants',
+        MaxImpactFundGrantSerializer,
+        _get_grant_by_donation_date)
+    return JsonResponse(response)
+
+def _construct_response(query_strings, klass: type, class_description: str, serializer, fetch_by_donation: Callable, extra_queries=Q()) -> dict:
+    dates = _get_dates(query_strings)
+    if dates.donation_year:
+        records = fetch_by_donation(klass, dates, query_strings)
+    else:
+        records = _get_records(klass, dates, extra_queries)
+
+    response = {class_description: [
+        serializer(record, context=query_strings).data for record in records]}
+    if not records:
+        response['warnings'] = [f'No {class_description} found with those parameters']
+    return response
+
+def _get_dates(query_strings) -> namedtuple:
+    Dates = namedtuple(
+        'Dates', 'start_year start_month end_year end_month donation_year donation_month donation_day')
+    return Dates(
+        query_strings.get('start_year') or 2000,
+        query_strings.get('start_month') or 1,
+        query_strings.get('end_year') or date.today().year,
+        query_strings.get('end_month') or 12,
+        query_strings.get('donation_year'),
+        query_strings.get('donation_month', 1),
+        query_strings.get('donation_day', 1))
+
+def _get_evaluations_by_donation_date(klass, dates, query_strings) -> list:
+    records = []
+
+    for abbreviation in _get_charity_abbreviations(query_strings):
+        single_charity_query = Q(charity__abbreviation=abbreviation)
+        try:
+            record = _get_record_by_donation_date(klass, dates, single_charity_query)
+            records += record
+        except IndexError:
+            pass
+    return records
+
+def _get_grant_by_donation_date(klass, dates, _query_strings):
+    return _get_record_by_donation_date(klass, dates)
+
+def _get_record_by_donation_date(klass, dates, q3=Q()) -> list:
+    q1 = Q(start_year=dates.donation_year, start_month__lte=dates.donation_month)
+    q2 = Q(start_year__lt=dates.donation_year)
+    return [klass.objects.filter(
+        (q1 | q2) & q3).order_by('-start_month', '-start_year')[0]]
+
+def _get_records(klass, dates, extra_queries):
+    return klass.objects.filter(
+        extra_queries,
         start_year__gte=dates.start_year,
         start_month__gte=dates.start_month,
         start_year__lte=dates.end_year,
         start_month__lte=dates.end_month)
-    response = {'max_impact_fund_grants': [
-        MaxImpactFundGrantSerializer(grant, context=queries).data for grant in grants]}
-    if not grants:
-        response['warnings'] = ['No grants found with those parameters']
-    return JsonResponse(response)
 
-def _get_dates(queries) -> dict:
-    '''Fill in missing dates from the request query, defaulting to
-    beginning of evaluations when start_dates are missing and present day
-    when end dates are missing'''
-    Dates = namedtuple(
-        'Dates', 'start_year start_month end_year end_month')
-    return Dates(
-        queries.get('start_year') or 2000,
-        queries.get('start_month') or 1,
-        queries.get('end_year') or date.today().year,
-        queries.get('end_month') or 12)
-
-
+def _get_charity_abbreviations(query_strings):
+    return [abbreviation.upper()
+            for abbreviation in query_strings.getlist('charity_abbreviation')] or (
+                Charity.objects.values_list('abbreviation', flat=True))
